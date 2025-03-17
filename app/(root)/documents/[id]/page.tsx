08138fd24bce37a59df15be70ca9b418c486bb64
Nare Sakhoyan <nare.sakhoyan@gmail.com>
2025-03-17 23:55:34 +0400

    setup, auth, editor integration, crete rooms with permissions

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,10 @@
+import CollaborativeRoom from '@/components/CollaborativeRoom';
+function Document() {
+  return (
+    <main className="flex w-full flex-col items-center">
+      <CollaborativeRoom />
+    </main>
+  );
+}
+
+export default Document;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,44 @@
+// 'use client';
+// rfce - reeact create a new component
+import Header from '@/components/header';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import AddDocumentBtn from '@/components/AddDocumentBtn';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+const Home = async () => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect('/sign-in');
+  const documents = [];
+  return (
+    <main className="home-container">
+      <Header className="sticky left-0 right-0">
+        <div className="flex item-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+      {documents.length > 0 ? (
+        <div></div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src="/assets/icons/doc.svg"
+            alt="document"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default Home;
